bede269d09b89c1e74f20230552a918fee1134e2
CyrilThebault <cyril.thebault@ucalgary.ca>
2026-07-20 15:26:09 +1200

    Remove deprecated deltim forcing option

--- build/FUSE_SRC/FUSE_ENGINE/force_info.f90
+++ build/FUSE_SRC/FUSE_ENGINE/force_info.f90
@@ -35,7 +35,6 @@ contains
  USE multiforce,only:vname_iy,vname_im,vname_id             ! names of time variables (day of year)
  USE multiforce,only:vname_ih,vname_imin,vname_dsec         ! names of time variables (time of day)
  USE multiforce,only:vname_dtime                            ! name of time variable (time since reference time)
- USE multiforce,only:deltim                                 ! model timestep (days)
  USE multiforce,only:istart,numtim_sim                      ! index for start of inference, and number steps in the reduced array
  USE multiforce,only:amult_ppt,amult_pet,amult_q            ! used to convert input flux units to FUSE internal units
  USE multiforce,only:numtim_sub                             ! number of time steps of subperiod (will be kept in memory)
@@ -61,7 +60,7 @@ contains
  integer(i4b)                           :: iend_data            ! end index of data in string charlines(iLine)
  character(len=strLen)                  :: cName,cData          ! name and data from charlines(iLine)
  ! internal: named variables
- integer(i4b),parameter                 :: maxinfo=22          ! maximum number of informational elements
+ integer(i4b),parameter                 :: maxinfo=21          ! maximum number of informational elements
  logical(lgt),dimension(maxinfo)        :: lCheck               ! vector to check that we have the infomation we need
  integer(i4b),parameter                 :: iVname_iy      =1    ! named variable for element of lCheck
  integer(i4b),parameter                 :: iVname_im      =2    ! named variable for element of lCheck
@@ -84,7 +83,6 @@ contains
  integer(i4b),parameter                 :: iUnits_swdown  =19   ! named variable for element of lCheck
  integer(i4b),parameter                 :: iUnits_potevap =20   ! named variable for element of lCheck
  integer(i4b),parameter                 :: iUnits_q       =21   ! named variable for element of lCheck
- integer(i4b),parameter                 :: iDeltim        =22   ! named variable for element of lCheck
  ! get units strings (used to define variable multipliers)
  character(len=strLen)                  :: units_aprecip='undefined' ! unit string for precipitation
  character(len=strLen)                  :: units_airtemp='undefined' ! unit string for air temperature
@@ -154,8 +152,6 @@ contains
    case('<units_swdown>');  units_swdown  = trim(cData);              lCheck(iUnits_swdown)   = .true.
    case('<units_potevap>'); units_potevap = trim(cData);              lCheck(iUnits_potevap)  = .true.
    case('<units_q>');       units_q       = trim(cData);              lCheck(iUnits_q)        = .true.
-   ! put real numbers and integers in their correct place
-   case('<deltim>');     read(cData,*,iostat=ierr) deltim;            lCheck(iDeltim)         = .true.
    ! check for an unexpected string
    case default
     ierr=20; message=trim(message)//'do not have a case for string ['//trim(cName)//']'; return
@@ -194,7 +190,6 @@ contains
   write(*,'(a,1x,a,1x,L1)')    '<units_swdown>',  trim(units_swdown),  lCheck(iUnits_swdown)
   write(*,'(a,1x,a,1x,L1)')    '<units_potevap>', trim(units_potevap), lCheck(iUnits_potevap)
   write(*,'(a,1x,a,1x,L1)')    '<units_q>',       trim(units_q),       lCheck(iUnits_q)
-  write(*,'(a,1x,f9.6,1x,L1)') '<deltim>',        deltim,              lCheck(iDeltim)
   print*, lCheck, size(lcheck)
   return
  endif  ! if we missed a variable
