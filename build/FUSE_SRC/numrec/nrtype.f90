945f7cc5bd9db29fc7531ff6ac1f9e01b75179f0
Martyn Clark <martyn.clark@ucalgary.ca>
2026-07-28 10:17:54 -0600

    Introduce working precision in Numerical Recipes

--- build/FUSE_SRC/numrec/nrtype.f90
+++ build/FUSE_SRC/numrec/nrtype.f90
@@ -1,23 +1,27 @@
 MODULE nrtype
-use kinds_dmsl_kit_FUSE,only:mrk
   INTEGER, PARAMETER :: I4B = SELECTED_INT_KIND(9)
   INTEGER, PARAMETER :: I2B = SELECTED_INT_KIND(4)
   INTEGER, PARAMETER :: I1B = SELECTED_INT_KIND(2)
   INTEGER, PARAMETER :: DP  = KIND(1.d0)
-  INTEGER, PARAMETER :: SP = mrk ! KIND(1.d0)  ! link to kinds to avoid conflicts
-  INTEGER, PARAMETER :: MSP = KIND(1.0)  ! SP still needed for f77 netcdf routines
+  !INTEGER, PARAMETER :: SP  = KIND(1.0)
+  INTEGER, PARAMETER :: SP  = DP
   INTEGER, PARAMETER :: SPC = KIND((1.0,1.0))
   INTEGER, PARAMETER :: LGT = KIND(.true.)
   integer, parameter :: I8B = selected_int_kind(15)
   integer, parameter :: DPC = kind((1.0D0,1.0D0))
 
+  ! define specific precision
+  integer, parameter :: WP  = DP ! working precision
+  INTEGER, PARAMETER :: MSP = KIND(1.0)  ! SP still needed for f77 netcdf routines
+
+  ! define constants
   REAL(SP), PARAMETER :: PI_SP=3.141592653589793238462643383279502884197_sp
   REAL(SP), PARAMETER :: PIO2=1.57079632679489661923132169163975144209858_sp
   REAL(SP), PARAMETER :: TWOPI=6.283185307179586476925286766559005768394_sp
   REAL(SP), PARAMETER :: SQRT2=1.41421356237309504880168872420969807856967_sp
   REAL(SP), PARAMETER :: EULER=0.5772156649015328606065120900824024310422_sp
 
-
+  ! define string lengths
   integer(i4b), parameter :: strLen     = 256
   integer(i4b), parameter :: FileStrLen = 300
   integer(i4b), parameter :: gageStrLen = 30
